15b4e4223f04a86e3334dc21ebe78bb95b1f2ec6
Pooja Pandey <pooja0707pandey@gmail.com>
2025-05-16 23:53:59 +0530

    Add files via upload

---
+++ Lambda/Interface/Add.java
@@ -0,0 +1,6 @@
+package Interface;
+
+@FunctionalInterface
+public interface Add {
+    void add(int a,int b);  
+}
---
+++ Lambda/Interface/Product.java
@@ -0,0 +1,6 @@
+package Interface;
+
+@FunctionalInterface
+public interface Product {
+    void product(int a,int b);
+}
